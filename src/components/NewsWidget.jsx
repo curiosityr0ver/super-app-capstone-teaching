efaa238b1f5f80ffb364a3b7f412ccaff77ba324
Ishu Mehta <mehta.ishu14@gmail.com>
2024-04-17 20:35:53 +0530

    refractored news and weather widget

--- src/components/NewsWidget.jsx
+++ src/components/NewsWidget.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import styles from "./NewsWidget.module.css";
 
-function NewsWidget() {
-	return <div>NewsWidget</div>;
+function NewsWidget({ news }) {
+	console.log(news);
+	const formatDate = (date) => {
+		if (date) {
+			const formattedDate = new Date(news.publishedAt).toLocaleDateString(
+				"en-US",
+				{
+					year: "numeric",
+					month: "long",
+					day: "numeric",
+				}
+			);
+			const formattedTime = new Date(news.publishedAt).toLocaleTimeString(
+				"en-US",
+				{
+					hour: "numeric",
+					minute: "numeric",
+					hour12: true,
+				}
+			);
+
+			return `${formattedDate} ${formattedTime}`;
+		} else {
+			const formattedDate = new Date().toLocaleDateString("en-US", {
+				year: "numeric",
+				month: "long",
+				day: "numeric",
+			});
+			const formattedTime = new Date().toLocaleTimeString("en-US", {
+				hour: "numeric",
+				minute: "numeric",
+				hour12: true,
+			});
+
+			return `${formattedDate} ${formattedTime}`;
+		}
+	};
+
+	return (
+		<div className={styles.newsWidget}>
+			<div className={styles.header}>
+				<img src={news.urlToImage} alt="" />
+				<div className={styles.headerText}>
+					<h3>{news.title.split("-")[0]}</h3>
+					<h5>{formatDate(news.publishedAt)}</h5>
+				</div>
+			</div>
+			<div className={styles.footer}>{news.description}</div>
+		</div>
+	);
 }
 
 export default NewsWidget;
--- src/components/WeatherWidget.jsx
+++ src/components/WeatherWidget.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./WeatherWidget.module.css";
+import { FaThermometerThreeQuarters } from "react-icons/fa";
+import { FiWind } from "react-icons/fi";
+import { RiContrastDrop2Fill } from "react-icons/ri";
+import axios from "axios";
 
 function WeatherWidget() {
-	return <div>WeatherWidget</div>;
+	const WEATHER_API = process.env.REACT_APP_WEATHER_API_KEY;
+	const [weather, setWeather] = useState();
+
+	useEffect(() => {
+		// console.log(new Date());
+		// setSelectedGenres(JSON.parse(localStorage.getItem("selectedGenres")));
+		fetchWeatherData();
+	}, []);
+	const fetchWeatherData = async () => {
+		const { data, status } = await axios.get(
+			`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API}&q=Mumbai`
+		);
+		if (status == 200) {
+			setWeather(data.current);
+		}
+	};
+
+	const formatDate = () => {
+		const formattedDate = new Date().toLocaleDateString("en-US", {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+		});
+		const formattedTime = new Date().toLocaleTimeString("en-US", {
+			hour: "numeric",
+			minute: "numeric",
+			hour12: true,
+		});
+
+		return [formattedDate, formattedTime];
+	};
+
+	if (weather)
+		return (
+			<div className={styles.widget}>
+				<div className={styles.header}>
+					<h1>{formatDate()[0]}</h1>
+					<h1>{formatDate()[1]}</h1>
+				</div>
+				<div className={styles.footer}>
+					<div className={styles.column}>
+						<img src={weather.condition.icon} alt="" />
+						<div>{weather.condition.text}</div>
+					</div>
+					<div className={styles.column}>
+						<div style={{ fontSize: "3rem" }}>{weather.temp_c} °C </div>
+						<div>
+							<FaThermometerThreeQuarters className={styles.icon} />
+							<div>{weather.pressure_mb} mbar</div>
+						</div>
+						<div>Pressure</div>
+					</div>
+					<div className={styles.column}>
+						<div>
+							<FiWind className={styles.icon} />
+							{weather.wind_kph} km/h
+						</div>
+						<div>Wind</div>
+						<div>
+							<RiContrastDrop2Fill className={styles.icon} />
+							<div>{weather.humidity}% </div>
+						</div>
+						<div>Humidity</div>
+					</div>
+				</div>
+			</div>
+		);
 }
 
 export default WeatherWidget;
